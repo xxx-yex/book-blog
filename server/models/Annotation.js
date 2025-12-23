5026d3bad13c1e39446d2c9eada91f8e4be1518b
2301 80186749 <2301_80186749@noreply.gitcode.com>
2025-12-23 18:17:16 +0800

    修复注释不匹配的问题

--- server/models/Annotation.js
+++ server/models/Annotation.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const AnnotationSchema = new mongoose.Schema({
   article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
-  selectedText: { type: String, required: true }, // 选中的文本
-  startOffset: { type: Number, required: true }, // 在HTML内容中的起始位置
-  endOffset: { type: Number, required: true }, // 在HTML内容中的结束位置
-  comment: { type: String, required: true }, // 注释/说明内容
+  selectedText: { type: String, required: true },
+  startOffset: { type: Number, required: true },
+  endOffset: { type: Number, required: true },
+  comment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
@@ -16,4 +16,3 @@ AnnotationSchema.pre('save', function(next) {
 });
 
 module.exports = mongoose.model('Annotation', AnnotationSchema);
-
